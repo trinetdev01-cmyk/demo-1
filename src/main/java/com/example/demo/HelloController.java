bf7962f9fcb6807cc23966c5ae860edb30e847a3
trinet_dev01 <trinet_dev01@trinet>
2026-01-05 06:08:53 +0900

    first commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class HelloController {
+
+    @GetMapping("/test")
+    public String test() {
+        log.info("★★★ ログ出力のテストに成功しました ★★★");
+        return "Spring Boot 起動成功！";
+    }
+}
